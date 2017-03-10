729e779c828d22299025e0f430a6c6e95ae01314
Alejandro Romero <arome90@gmail.com>
2017-03-10 15:40:44 +0100

    Lanzador de conversacion

---
+++ HackForGood/Assets/Scripts/conversationLaunch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class conversationLaunch : MonoBehaviour {
+
+	private bool characterIn;
+	public Text textoExplicativo;
+	public string conversation;
+	// Use this for initialization
+	void Start () {
+		this.textoExplicativo.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (characterIn) {
+			if (Input.GetKeyDown ("a")) {
+				Debug.Log (conversation);
+			}
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col) {
+		characterIn = true;
+		this.textoExplicativo.enabled = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		characterIn = false;
+		this.textoExplicativo.enabled = false;
+	}
+}
