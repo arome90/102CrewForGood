f4f105a7239bc95cb307d8a5212f190bef8d3b4c
Lolo <manuel.gonzalez@ucm.es>
2017-03-11 03:44:48 +0100

    LoadScene y cosas mas

---
+++ HackForGood/Assets/Scripts/MarcoGestion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class MarcoGestion : MonoBehaviour {
+
+    public GameObject anio;
+    public GameObject ciudad;
+
+    private Animator anioAnim;
+    private Animator ciudadAnim;
+
+    void Start()
+    {
+        this.anioAnim = this.anio.GetComponent<Animator>();
+        this.ciudadAnim = this.ciudad.GetComponent<Animator>();
+    }
+
+    public void ActivarAnio()
+    {
+        this.anioAnim.SetBool("Aparecer", true);
+    }
+
+    public void ActivarCiudad()
+    {
+        this.ciudadAnim.SetBool("Aparecer", true);
+    }
+}
