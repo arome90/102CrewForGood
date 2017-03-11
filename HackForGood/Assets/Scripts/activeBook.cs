7f9afb81c5baf4e7186b8fa597e1c2f1b0e707d9
César Díaz-Faes Pérez <cdiazfaes@gmail.com>
2017-03-11 05:26:46 +0100

    mas efectos de sonido y arreglo de bugs de LUA

--- HackForGood/Assets/Scripts/activeBook.cs
+++ HackForGood/Assets/Scripts/activeBook.cs
@@ -4,21 +4,28 @@ public class activeBook : MonoBehaviour {
 
     private Animator anim;
     private conversationLaunch converLaunch;
+    private AudioSource aud;
 
     public GameObject flujoConver;
+    public AudioClip teleportEffect;
 
 	void Start () {
         this.anim = this.gameObject.GetComponent<Animator>();
         this.converLaunch = this.flujoConver.GetComponent<conversationLaunch>();
+        this.aud = this.gameObject.GetComponent<AudioSource>();
 	}
 
     public void activeBorrar()
     {
+        this.aud.Play();
         this.anim.SetBool("Borrar", true);
     }
 
     public void activeAbsorber()
     {
+        this.aud.Stop();
+        this.aud.clip = this.teleportEffect;
+        this.aud.Play();
         this.anim.SetBool("Absorber", true);
     }
 
