30283dd4e2ac5b4d45f1a14e13d40f206f59d7a4
César Díaz-Faes Pérez <cdiazfaes@gmail.com>
2017-03-11 10:27:47 +0100

    ultimos cambios

---
+++ HackForGood/Assets/Scripts/GameManagerBoatMinigame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerBoatMinigame : MonoBehaviour
+{
+    public GameObject boatPlayer;
+    public GameObject boatPlayer2;
+    public GameObject boatPlayer3;
+
+    private moveBoat movePlayerScript;
+    private moveBoat movePlayerScript2;
+    private moveBoat movePlayerScript3;
+    private bool blockDialogue = false;
+
+    void Start()
+    {
+        this.movePlayerScript = this.boatPlayer.GetComponent<moveBoat>();
+        this.movePlayerScript2 = this.boatPlayer2.GetComponent<moveBoat>();
+        this.movePlayerScript3 = this.boatPlayer3.GetComponent<moveBoat>();
+    }
+
+
+    public void setCanMove()
+    {
+        this.movePlayerScript.setCanMove();
+        this.movePlayerScript2.setCanMove();
+        this.movePlayerScript3.setCanMove();
+    }
+
+    public void setCanNotMove()
+    {
+        this.movePlayerScript.setCanNotMove();
+        this.movePlayerScript2.setCanMove();
+        this.movePlayerScript3.setCanMove();
+    }
+}
+
