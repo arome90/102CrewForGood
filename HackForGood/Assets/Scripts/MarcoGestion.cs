cbd7062491e8c2a4302411d3ce39cd54eadd145c
César Díaz-Faes Pérez <cdiazfaes@gmail.com>
2017-03-11 04:49:08 +0100

    Musica en escena 2 y 3

--- HackForGood/Assets/Scripts/MarcoGestion.cs
+++ HackForGood/Assets/Scripts/MarcoGestion.cs
@@ -8,20 +8,29 @@ public class MarcoGestion : MonoBehaviour {
 
     private Animator anioAnim;
     private Animator ciudadAnim;
+    private AudioSource aud;
 
     void Start()
     {
         this.anioAnim = this.anio.GetComponent<Animator>();
         this.ciudadAnim = this.ciudad.GetComponent<Animator>();
+        this.aud = this.gameObject.GetComponent<AudioSource>();
     }
 
     public void ActivarAnio()
     {
+        this.playEffect();
         this.anioAnim.SetBool("Aparecer", true);
     }
 
     public void ActivarCiudad()
     {
+        this.playEffect();
         this.ciudadAnim.SetBool("Aparecer", true);
+    }
+
+    public void playEffect()    //En eventos
+    {
+        this.aud.Play();
     }
 }
